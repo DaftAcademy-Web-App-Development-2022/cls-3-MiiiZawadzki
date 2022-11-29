74b9de9d409d5337d779bcb7b92c92534d9d7626
MiiiZawadzki <zawadzki.micha@gmail.com>
2022-11-29 21:50:15 +0100

    implement endpoints

--- src/pages/api/playlist/[id].ts
+++ src/pages/api/playlist/[id].ts
@@ -1,12 +1,41 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { dbConnect } from "~/libraries/mongoose.library";
+import { Playlist } from "~/models/Playlist.model";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Response>
 ) {
+  const { id } = req.query;
   await dbConnect();
-  res.status(200).send({});
+
+  if (req.method === "GET") {
+    const playlist = await getPlaylist(id as string);
+    res.status(200).send({ data: playlist });
+  }
+  else if (req.method === "DELETE") {
+    await deletePlaylist(id as string);
+    res.status(200).send({ data: null });
+  }
+}
+
+async function getPlaylist(id: string) {
+  const result = await Playlist.findById(id);
+  if (result === null) return null;
+  const playlist = result.toObject();
+  return {
+    name: playlist.name,
+    owner: playlist.owner,
+    slug: playlist.slug,
+    spotifyId: playlist.spotifyId,
+    upvote: playlist.upvote,
+    color: playlist.color,
+    id: playlist._id.toString(),
+  };
+}
+
+async function deletePlaylist(id: string) {
+  await Playlist.findByIdAndDelete(id);
 }
 
 export type Response = any;
--- src/pages/api/playlist/index.ts
+++ src/pages/api/playlist/index.ts
@@ -1,12 +1,46 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { dbConnect } from "~/libraries/mongoose.library";
+import { Playlist } from "~/models/Playlist.model";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<any>
 ) {
   await dbConnect();
-  res.status(200).send({});
+
+  if (req.method === "GET") {
+    const playlists = await getPlaylists()
+    res.status(200).send({ data: playlists });
+  }
+  else if (req.method === "POST") {
+    const result = await createPlaylist(req.body);
+    const response = mapper(result);
+    res.status(201).send({ data: response });
+  }
 }
 
+async function getPlaylists() {
+  const playlists = await Playlist.find();
+  return playlists.map((dbRecord) => {
+    const playlist = dbRecord.toObject();
+    return mapper(playlist);
+  });
+}
+
+async function createPlaylist(object: unknown) {
+  const playlist = await Playlist.create(object);
+  return playlist;
+}
+
+function mapper(playlist: any) {
+  return {
+    name: playlist.name,
+    owner: playlist.owner,
+    slug: playlist.slug,
+    spotifyId: playlist.spotifyId,
+    upvote: playlist.upvote,
+    color: playlist.color,
+    id: playlist._id.toString(),
+  };
+}
 export type Response = any;
